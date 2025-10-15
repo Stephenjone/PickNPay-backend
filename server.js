6bba21e3f1ed54141262b97fbb4cf03666b37d67
Your GitHub Name <your_github_email@example.com>
2025-10-15 10:29:24 +0530

    Fix wildcard route for Express production

--- server.js
+++ server.js
@@ -91,10 +91,12 @@ io.on('connection', (socket) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client', 'build')));
 
-  app.get('*', (req, res) => {
+  // Regex fallback to catch all unmatched routes
+  app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
 
 
 // Global error handler
